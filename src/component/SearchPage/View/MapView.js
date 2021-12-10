230eeee0a8f071378fb1c46fefd6c1d5e5fac3a0
inyoung-seo <inyeongseo20@gmail.com>
2021-12-10 10:15:04 +0900

    feat:searchForm Update

--- src/component/SearchPage/View/MapView.js
+++ src/component/SearchPage/View/MapView.js
@@ -5,37 +5,80 @@
 import React, {useRef,useEffect} from "react";
 import styled from 'styled-components';
 
+const markerdata =[
+    {
+      title: "클럽에반스",
+      lat: 37.55033897015321,
+     lng: 126.9229336137093, 
+    },
+    {
+      title: "치차티클럽",
+      lat: 37.57109940534566,lng: 127.01135026952784, 
+    },
+    {
+      title: "TRVR Cafe",
+      lat:37.53838839800507, 
+      lng: 126.99353635603627,
+    },
+    {
+      titel:"용용선생",
+      lat:37.55939806463767, 
+      lng: 127.03980732534552,
+    },
+  ];
+  
 const MapViewStyle = styled.div `
-.map {
+#map {
     width: 65%;
     height:500px;
     margin: 0 auto;
 }
 @media screen and (max-width:958px ) {
-    .map {
+    #map {
         width: 95%;
     }
 }
 `;
-const options = {
-    //지도를 생성할 때 필요한 기본 옵션
-    center: new window.kakao.maps.LatLng(37.56284927225243, 126.97138694483239), //지도의 중심좌표.
-    level: 6, //지도의 레벨(확대, 축소 정도)
-  };
 
-function MapView () {
-    const container = useRef(null);
+function Map() {
     useEffect(() => {
-        new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
-        return () => {};
-      }, []);
-    return (
-        <MapViewStyle>
-            <div className="map" ref={container}>
-            </div>
-        </MapViewStyle>
-        
-    );
-}
+      mapscript();
+    }, []);
+  
+    const mapscript = () => {
+      let container = document.getElementById("map");
+      let options = {
+        center: new window.kakao.maps.LatLng(37.56284927225243, 126.97138694483239),
+        level: 5,
+      };
+  
+      //map
+      const map = new window.kakao.maps.Map(container, options);
+      
+      markerdata.forEach((el) => {
+        // 마커를 생성합니다
+        new window.kakao.maps.Marker({
+          //마커가 표시 될 지도
+          map: map,
+          //마커가 표시 될 위치
+          position: new window.kakao.maps.LatLng(el.lat, el.lng),
+          //마커에 hover시 나타날 title
+          title: el.title,
+        });
+      });
+    };
+  
+    return <div id="map"></div>;
+  }
 
-export default MapView;
+  function MapView(){
+      return(
+          <center>
+        <MapViewStyle>
+        <Map/>
+        </MapViewStyle>
+        </center>
+      );
+  }
+  
+  export default MapView;
